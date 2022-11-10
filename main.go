4ca8fce9b837fb6f33d9a4ba30a0dd79256b8a04
lynx <1044945158@qq.com>
2022-11-10 22:45:04 +0800

    feature: Complete instruction input reading

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "log"
+import (
+	"1314liuwei/sqlite.go/consts"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
 
 func main() {
-	log.Println("Hello, Sqlite-go!")
+	fmt.Println("Hello, Sqlite-go!")
+
+	var (
+		err         error
+		input       string
+		inputReader = bufio.NewReader(os.Stdin)
+	)
+
+	for {
+		fmt.Printf("db>")
+		input, err = inputReader.ReadString('\n')
+		if err != nil {
+			log.Fatalf("input err: %s", err)
+		}
+		input = strings.TrimSpace(input)
+
+		if input == consts.Exit {
+			return
+		} else {
+			fmt.Printf("Unrecognized command '%s'. \n", input)
+		}
+	}
 }
